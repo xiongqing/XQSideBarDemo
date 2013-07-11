0cfd51f48e8fd0a3a5a5d8931aa724db97ac4f18
HySoft <hysoft@HySofttekiiMac.local>
2013-07-11 16:23:21 +0800

    Initial Commit

---
+++ XQSideBarDemo/XQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XQAppDelegate.h
+//  XQSideBarDemo
+//
+//  Created by HySoft on 13-7-11.
+//  Copyright (c) 2013年 HySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XQSideBarDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XQSideBarDemo
+//
+//  Created by HySoft on 13-7-11.
+//  Copyright (c) 2013年 HySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XQAppDelegate class]));
+    }
+}
